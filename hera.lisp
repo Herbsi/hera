7c8e125242bfa467cd021a9980da334259ba6a43
Herwig Hoehenberger <herwig.hoehenberger@gmail.com>
2020-12-27 14:22:55 +0100

    Version 2.0; in Common Lisp

--- hera.lisp
+++ hera.lisp
@@ -2,6 +2,12 @@
 
 (in-package #:hera)
 
+(defparameter *recipe-root* #P"~/Notes/Recipes/")
+(defparameter *apple-notes-id*  "x-coredata://F93C847E-0906-4FFC-881E-472B69BB9B9C/ICNote/p261")
+(defparameter *omnifocus-project* "🏡 Housework")
+(defparameter *reminders-list* "Grocery")
+(defparameter *tmp-file* "/tmp/ingredients")
+
 (defun transform-meal (meal-list mealplan day)
   "Transform one (<meal> args) list into a
 
@@ -24,14 +30,14 @@ into a list of (add-meal (make-meal <meal> args) <day>) function calls."
     `(let ((,mealplan (make-mealplan)))
        ,@(mappend (fn (day-list) (transform-day day-list mealplan))
                   body)
-       (mealplan-add-to-apple-notes ,mealplan "x-coredata://F93C847E-0906-4FFC-881E-472B69BB9B9C/ICNote/p261")
-       (mealplan-add-to-omnifocus ,mealplan "🏡 Housework"))))
+       (mealplan-add-to-apple-notes
+        ,mealplan ,*apple-notes-id*)
+       (mealplan-add-to-omnifocus
+        ,mealplan ,*omnifocus-project*)
+       (append-list-to-file ,*tmp-file*
+        (collect-ingredients ,mealplan))
+       (format t "~&Edit ~a and press Enter once done~%> ~%" ,*tmp-file*)
+       (read-line)
+       (mapc (fn (item) (add-item-to-reminders item ,*reminders-list*))
+             (read-list-from-file)))))
 
-(defparameter *recipe-root* #P"~/Notes/Recipes/")
-
-;; (hera
-;;   (Monday
-;;    ("Spätzlepfanne mit Rosenkohl.md")
-;;    ("Schoko Gugelhupf.md" :kind :bake :apple-notes nil))
-;;   (Tuesday
-;;    ("Schoko-Mandel Kuchen.md" :kind :bake)))
